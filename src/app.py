b63b7fd3abd3ff108fe724c9c8046312e2635ee1
Fernando Cavarzare <fernando.cavarzare@gmail.com>
2026-02-26 19:16:48 -0300

    Criacao de firewall, senha criptografada, pdb, etc

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 import os
 import pymssql
-import time
 
 from fastapi import FastAPI, Form, Request, HTTPException
 from fastapi.responses import HTMLResponse, JSONResponse
@@ -43,13 +42,6 @@ def get_conn(timeout: int = 5) -> pymssql.Connection:
 def healthz():
     print("Healthcheck requested")
     return {"status": "ok"}
-
-
-@app.get("/stress")
-def stress():
-    # Simula uma operação pesada de 1.1 segundos
-    time.sleep(1.1)
-    return {"status": "stressed"}
 
 
 # Readiness: garante que a app consegue falar com o SQL
